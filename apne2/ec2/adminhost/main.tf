d0e3ea29b1ff5a704b2a8d9e8714a5c90ac30475
jump99500 <jump99500@gamil.com>
2024-01-17 07:43:26 +0000

    keypair changes

--- apne2/ec2/adminhost/main.tf
+++ apne2/ec2/adminhost/main.tf
@@ -5,7 +5,7 @@ module "adminhost" {
   create_adminhost       = true
   ami                    = var.ami
   instance_type          = var.instance_type
-  key_name               = "aws_k8s_test"
+  key_name               = "jeewong-eks-studykey"
   subnet_id              = data.terraform_remote_state.vpc.outputs.subnet_ids[0]
   vpc_security_group_ids = [data.terraform_remote_state.sg.outputs.admin_sg_id]
   user_data_base64       = base64encode(local.user_data)
