c2015612db5b75406b2e55b6d9579141e92ed6cc
jump99500 <jump99500@gamil.com>
2024-01-22 02:21:02 +0000

    Create aws-auth-cm

--- apne2/cluster/aws-auth-cm/locals.tf
+++ apne2/cluster/aws-auth-cm/locals.tf
@@ -12,7 +12,7 @@ locals {
 
   mapRoles = [
     {
-      rolearn  = "arn:aws:iam::144149479695:role/Jeewoong-k8s-infra-admin-profile"
+      rolearn  = "arn:aws:iam::144149479695:role/Jeewoong-k8s-infra-admin-role"
       username = "Jeewoong-k8s-infra-admin-role"
       groups   = ["system:masters"]
     }
