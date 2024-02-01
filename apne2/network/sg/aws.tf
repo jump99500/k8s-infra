28b0cfc0f2492da87754fbdfed15e5843458f17b
jump99500 <jump99500@gamil.com>
2024-02-01 06:11:33 +0000

    Create SG

--- apne2/network/sg/aws.tf
+++ apne2/network/sg/aws.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   allowed_account_ids = var.account_id
-  region              = var.region}
+  region              = var.region
+  }
 
 variable "account_id" {
   default = ["144149479695"]
