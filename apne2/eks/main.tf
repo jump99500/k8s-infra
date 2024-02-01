61a5de775ba4593397ac02a5594f7f8dca252ec8
jump99500 <jump99500@gamil.com>
2024-02-01 06:40:53 +0000

    Create EKS Cluster

--- apne2/eks/main.tf
+++ apne2/eks/main.tf
@@ -1,6 +1,7 @@
 module "eks" {
   source = "../../module/eks"
 
+
   name                    = "Jeewoong-k8s-infra"
   eks_version             = var.eks_version
   vpc_subnet_ids          = [data.terraform_remote_state.vpc.outputs.subnet_ids[0], data.terraform_remote_state.vpc.outputs.subnet_ids[1]]
