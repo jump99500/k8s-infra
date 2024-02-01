f3a087205da889deb48cf3137356e251b8d1ede1
jump99500 <jump99500@gamil.com>
2024-02-01 04:26:09 +0000

    Modify auth-cm code

--- module/cluster/iam.tf
+++ module/cluster/iam.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role_policy_attachment" "eks_role_policy" {
+resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-data "aws_iam_policy_document" "policy_trust_relationships" {
+data "aws_iam_policy_document" "policy_document" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "policy_trust_relationships" {
 
 resource "aws_iam_role" "eks_role" {
   name               = format("${var.name}-%s-%s", "eks", "role")
-  assume_role_policy = data.aws_iam_policy_document.policy_trust_relationships.json
+  assume_role_policy = data.aws_iam_policy_document.policy_document.json
 
   tags = {
     Name = format("${var.name}-%s-%s", "eks", "role")
--- module/cluster/outputs.tf
+++ module/cluster/outputs.tf
@@ -1,15 +1,15 @@
 output "eks_id" {
-  value = concat(aws_eks_cluster.eks[*].id, [""])[0]
+  value = aws_eks_cluster.eks.id
 }
 
 output "eks_endpoint" {
-  value = concat(aws_eks_cluster.eks[*].endpoint, [""])[0]
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "eks_ca_certificate" {
-  value = concat(aws_eks_cluster.eks[*].certificate_authority[0].data, [""])[0]
+  value = aws_eks_cluster.eks.certificate_authority.0.data
 }
 
 output "eks_oidc" {
-  value = concat(aws_eks_cluster.eks[*].identity[0].oidc[0].issuer, [""])[0]
+  value = flatten(aws_eks_cluster.eks[*].identity[*].oidc.0.issuer)[0]
 }
