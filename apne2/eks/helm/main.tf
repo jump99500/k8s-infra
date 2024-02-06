de24cfa892460f752effd04b4383bc60adcf8eba
sangun <holic1208@daum.net>
2024-02-06 16:53:31 +0900

    add namespace

--- apne2/eks/helm/main.tf
+++ apne2/eks/helm/main.tf
@@ -2,6 +2,7 @@ resource "helm_release" "metrics-server" {
   name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "metrics-server"
+	namespace  = "kube-system"
   version    = "6.9.1"
 
   set {
